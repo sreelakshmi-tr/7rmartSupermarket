af1e60beb089c4c2f2ce00f844af8c8d53ecd427
Sreelakshmi <sreetr1119@gmail.com>
2024-01-30 22:33:14 +0530

    Report generation

---
+++ 7rmartSupermarket/src/main/java/utilities/ExtentReportUtility.java
@@ -0,0 +1,19 @@
+package utilities;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportUtility {
+	
+	public static final ExtentReports extentReports = new ExtentReports();
+
+	public synchronized static ExtentReports createExtentReports() {
+		ExtentSparkReporter reporter = new ExtentSparkReporter("./extent-reports/extent-report.html");
+		reporter.config().setReportName("SevenRmart Project");
+		extentReports.attachReporter(reporter);
+		extentReports.setSystemInfo("Organization", "Obsqura");
+		extentReports.setSystemInfo("Name", "Angitha Maria Thomas");
+		return extentReports;
+	}
+
+}
--- 7rmartSupermarket/src/test/java/testscript/Base.java
+++ 7rmartSupermarket/src/test/java/testscript/Base.java
@@ -2,15 +2,29 @@ package testscript;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class Base {
 	public WebDriver driver;
-	@BeforeMethod
-	public void inititializeBrowser() {
+	
+	@BeforeMethod 
+	@Parameters("browser")
+	public void inititializeBrowser(String browser) throws Exception {
 		
-		driver = new ChromeDriver();
+		
+		if(browser.equalsIgnoreCase("Chrome")) {
+		  driver = new ChromeDriver();
+		}else if(browser.equalsIgnoreCase("Edge")) {
+			driver = new EdgeDriver();
+		}else if(browser.equalsIgnoreCase("Firefox")) {
+			driver = new FirefoxDriver();
+		}else {
+			throw new Exception("Browser is not correct");
+		}
 		
 		driver.get("https://groceryapp.uniqassosiates.com/admin/login");
 		
