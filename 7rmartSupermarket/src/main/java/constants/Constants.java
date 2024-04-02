4409c34057b163b781de1a8ca09cdbe55a4ed611
Sreelakshmi <sreetr1119@gmail.com>
2024-04-02 13:32:05 +0530

    property file corrected

--- 7rmartSupermarket/src/main/java/constants/Constants.java
+++ 7rmartSupermarket/src/main/java/constants/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 
 	public static final String OFFERIMAGE = System.getProperty("user.dir")+ "\\src\\test\\resources\\code.png";
 
-	public static final String CONFIGFILE =System.getProperty("user.dir")+"\\src\\main\\resources\\config,properties";
+	public static final String CONFIGFILE =System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties";
 
 }
